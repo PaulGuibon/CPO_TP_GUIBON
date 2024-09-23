a459f9cf4c614bddae42264e2be50b4a73a810db
33783 <33783@10.3.60.148>
2024-09-23 16:12:28 +0200

    "Création d'un nouveau projet versionné"

--- TP0/Exo1/src/exo1/Exo1.java
+++ TP0/Exo1/src/exo1/Exo1.java
@@ -1,22 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+/*Paul Guibon
+TDA
+23/09*/
+ 
 package exo1;
-import java.itil.Scanner;
+
 import java.util.Scanner;
+
 /**
  *
  * @author 33783
  */
 public class Exo1 {
-System.out.println("Bonjour");
-String prenom;
-Scanner sc;
-sc = new Scanner(System.in);
-System.out.println("Bonjour, comment vous appelez-vous ?");
-String = sc.nextLine();
-System.out.println("Au-revoir");
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic heres
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+        System.out.println("Au revoir "+prenom);
     }
+    
 }

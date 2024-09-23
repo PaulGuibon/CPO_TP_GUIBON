8c41f67780540499a2bad2bb21928a67c0cb4dbb
33783 <33783@10.3.60.148>
2024-09-23 16:39:33 +0200

    "Création d'un nouveau projet versionné"

---
+++ TP0/Calculator/src/calculator/Calculator.java
@@ -0,0 +1,17 @@
+/*Paul Guibon
+TDA 
+TP0 Partie 3*/
+
+package calculator;
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Please enter the operator :\n1)add \n2)substract \n3multiply \n4)divide \n5)modulo");
+        
+    }
+    
+}

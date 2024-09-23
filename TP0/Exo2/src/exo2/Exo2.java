ee5c6187b46487edd15978fb2efc34c64df3d288
33783 <33783@10.3.60.148>
2024-09-23 16:26:14 +0200

    "Création d'un nouveau projet versionné"

---
+++ TP0/Exo2/src/exo2/Exo2.java
@@ -0,0 +1,35 @@
+/*Paul Guibon 
+TDA
+23/09*/
+
+package exo2;
+
+/**
+ *
+ * @author 33783
+ */
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+        nb=5;
+        result=0;
+        // Addition des nb premiers entiers
+        ind=1;
+        while (ind <= nb) {
+        result=result+ind;
+        ind = ind + 1;
+        }
+        // Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des "+ nb + " entiers est: "+result);
+        }
+    }
+    
+}

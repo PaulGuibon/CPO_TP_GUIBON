8a6b2c9acd7bb2c9cd9b0328cfbd3f866416fd13
33783 <33783@10.3.60.148>
2024-09-23 10:47:35 +0200

    "Création d'un nouveau projet versionné"

---
+++ TP0/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+import java.itil.Scanner;
+import java.util.Scanner;
+/**
+ *
+ * @author 33783
+ */
+public class Exo1 {
+System.out.println("Bonjour");
+String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, comment vous appelez-vous ?");
+String = sc.nextLine();
+System.out.println("Au-revoir");
+
+    }
+}

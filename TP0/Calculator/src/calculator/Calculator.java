c870060d58a717dc5e917cd6afbbcec6009cc228
33783 <33783@10.3.60.148>
2024-09-23 16:40:28 +0200

    "Création d'un nouveau projet versionné"

--- TP0/Calculator/src/calculator/Calculator.java
+++ TP0/Calculator/src/calculator/Calculator.java
@@ -3,6 +3,9 @@ TDA
 TP0 Partie 3*/
 
 package calculator;
+
+import java.util.Scanner;
+
 public class Calculator {
 
     /**
@@ -11,7 +14,7 @@ public class Calculator {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Please enter the operator :\n1)add \n2)substract \n3multiply \n4)divide \n5)modulo");
-        
+        Scanner op = new Scanner(System.in);
     }
     
 }

511b354c5197b1081577ecf682b7cbc6f7a44095
33783 <33783@10.3.60.148>
2024-09-27 17:14:50 +0200

    "Création d'un nouveau projet versionné"

--- TP0/TP1_convertisseur_GUIBON/src/tp1_convertisseur_guibon/TP1_convertisseur_GUIBON.java
+++ TP0/TP1_convertisseur_GUIBON/src/tp1_convertisseur_guibon/TP1_convertisseur_GUIBON.java
@@ -16,19 +16,67 @@ public class TP1_convertisseur_GUIBON {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Donner une temperature");
-        Scanner sc = new Scanner(System.in); 
-        double Celcius;
-        Celcius = sc.nextDouble();
-        System.out.println(Celcius+" degres Celcius");
-        double Kelvin;
-        Kelvin = Celcius + 273;
-        System.out.println(Kelvin+" Kelvin");
-        
+        System.out.println("Bonjour, saisissez une valeur :");
+        double valeur;
+        Scanner temp = new Scanner(System.in);
+        valeur = temp.nextInt();
+        System.out.println(valeur);
+        System.out.println("Saisissez la cerversion que vous saihaitez effectuer : \n1) Celcius vers Kelvin \n2) Kelvin vers celcius \n3) Farhenheit vers celcius \n4) Celcius vers Farhenheit vers celcius \n5 Kelvin vers farenheit \6 Farenheit vers kelvin");
+        int choix;
+        Scanner choice = new Scanner(System.in);
+        choix = choice.nextInt();
+        if (choix == 1){
+            CelciusVersKelvin (valeur);                    
+        }
+        else if (choix ==2){
+            KelvinVersCelcius(valeur);
+        }
+        else if (choix == 3) {
+            FarenheitVersCelcius(valeur);
+        }
+        else if (choix == 4) {
+            CelciusVerFarenheit(valeur);
+        }
+        else if (choix == 5) {
+            KelvinVerFarenheit(valeur);
+        }
+        else if (choix == 6) {
+            FarenheitVersKelvin(valeur);
+        }
     }
     public static double CelciusVersKelvin (double tCelcius){
         double Kelvin;
         Kelvin = tCelcius + 273;
+        System.out.println(Kelvin+" Kelvin");
+        return 0;
+    }
+    public static double KelvinVersCelcius(double tKelvin){
+        double Celcius;
+        Celcius = tKelvin-273;
+        System.out.println(Celcius+" Celcius");
+        return 0;
+    }
+    public static double FarenheitVersCelcius(double tFarenheit){
+        double Celcius;
+        Celcius = (tFarenheit -32) * 1.8;
+        System.out.println(Celcius+" Celcius");
+        return 0;
+    }
+    public static double CelciusVerFarenheit(double tCelcius){
+        double Farenheit;
+        Farenheit = (tCelcius -32) / 1.8;
+        System.out.println(Farenheit+" Farenheit");
+        return 0;
+    }
+    public static double KelvinVerFarenheit(double tKelvin){
+        double Farenheit;
+        Farenheit = (tKelvin - 273.15)*9/5+32;
+        System.out.println(Farenheit+" Farenheit");
+        return 0;
+    }
+    public static double FarenheitVersKelvin(double tFarenheit){
+        double Kelvin;
+        Kelvin = (tFarenheit-32)*5/9+ 273.15;
         System.out.println(Kelvin+" Kelvin");
         return 0;
     }

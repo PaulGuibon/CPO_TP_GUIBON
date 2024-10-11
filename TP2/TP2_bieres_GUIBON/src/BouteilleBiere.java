ebcb338ab1e9040f9c5e4afa8613c075121d5337
33783 <33783@10.3.60.148>
2024-10-11 11:07:45 +0200

    "Création d'un nouveau projet versionné"

---
+++ TP2/TP2_bieres_GUIBON/src/BouteilleBiere.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 33783
+ */
+public class BouteilleBiere {
+    // Attributs
+    String nom;          // Le nom de la bière
+    float degreAlcool;   // Le degré d'alcool de la bière
+    String brasserie;    // Le nom de la brasserie qui produit la bière
+    boolean ouverte;     // Indique si la bouteille est ouverte (true) ou non (false)
+}
+
